3b58044c766e60628b453d5d78a844cddee11bb4
hashimoto <test@sample.com>
2020-03-05 22:55:08 +0900

    Qiita2WPWebプロジェクト追加

---
+++ QiitaToWPWeb/Controllers/Qiita2WPWebController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using TestProject.QiitaToWP;
+
+namespace TestProject.QiitaToWPWeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class Qiita2WPWebController : ControllerBase
+    {
+        private readonly ILogger<Qiita2WPWebController> _logger;
+
+        public Qiita2WPWebController(ILogger<Qiita2WPWebController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            var q2wp = new Qiita2WP();
+
+            // Qiita2WPプロジェクトの方の処理を使いまわす
+            await q2wp.Qiita2WPArticle();
+
+            return Ok();
+        }
+    }
+}
